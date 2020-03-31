d26b29477d2fc4cc582d44b1c232305aad04249a
Armen Grigorian <info@propertystart.com.au>
2020-03-31 11:17:46 +1100

    Version 3

--- data.js
+++ data.js
@@ -24,29 +24,29 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -1.4340337197303175,
-        "pitch": -0.000257005847293712,
+        "yaw": -1.3560709631135612,
+        "pitch": 0.014983752496918967,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 1.7399073619765888,
-          "pitch": 0.11967757955273939,
-          "rotation": 2.356194490192345,
-          "target": "5-upstairs-living"
+          "yaw": 1.1251309982162532,
+          "pitch": 0.15411691575586062,
+          "rotation": 5.497787143782138,
+          "target": "1-livingfamily"
         },
         {
-          "yaw": 1.1492627551827663,
-          "pitch": 0.11689839559956638,
-          "rotation": 5.497787143782138,
-          "target": "1-living-room"
+          "yaw": 1.642035670075015,
+          "pitch": 0.30086891401789373,
+          "rotation": 1.5707963267948966,
+          "target": "4-upstairs-workspace"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-living-room",
-      "name": "Living Room",
+      "id": "1-livingfamily",
+      "name": "Living/Family",
       "levels": [
         {
           "tileSize": 256,
@@ -68,59 +68,28 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 2.4625969276273647,
-        "pitch": 0.21043118924898252,
+        "yaw": 1.9597345193774922,
+        "pitch": 0.13021319650800223,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 2.439950224562926,
-          "pitch": 0.1349560589623593,
-          "rotation": 0.7853981633974483,
+          "yaw": 2.4910354972695217,
+          "pitch": 0.09504615477761114,
+          "rotation": 7.0685834705770345,
           "target": "0-entrykitchen"
         },
         {
-          "yaw": 1.6188531429143627,
-          "pitch": 0,
-          "rotation": 7.853981633974483,
+          "yaw": 1.6547226404484299,
+          "pitch": 0.16502459354015286,
+          "rotation": 0.7853981633974483,
           "target": "5-upstairs-living"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-bathroom",
-      "name": "Bathroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "yaw": 1.8109380207522658,
-        "pitch": 0.11536183361283214,
-        "fov": 1.3219339076578436
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-rumpus",
+      "id": "2-rumpus",
       "name": "Rumpus",
       "levels": [
         {
@@ -143,16 +112,16 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -2.341405416872597,
-        "pitch": 0.02231168684087592,
+        "yaw": -2.4699271737595403,
+        "pitch": 0.01667910588907162,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "4-upstairs-workspace",
-      "name": "Upstairs Workspace",
+      "id": "3-bathroom-downstairs",
+      "name": "Bathroom downstairs",
       "levels": [
         {
           "tileSize": 256,
@@ -174,15 +143,52 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 2.0285957591363095,
-        "pitch": -0.1666242812369525,
+        "yaw": 2.0217782896669965,
+        "pitch": 0.19821089069695574,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-upstairs-workspace",
+      "name": "Upstairs workspace",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.1260741538557406,
+        "pitch": 0.1359206563800779,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 2.070895709029389,
-          "pitch": 0.24735578617731235,
-          "rotation": 0,
+          "yaw": 2.8632867720530646,
+          "pitch": 0.49478470362732097,
+          "rotation": 3.9269908169872414,
+          "target": "1-livingfamily"
+        },
+        {
+          "yaw": 2.021601851552492,
+          "pitch": 0.12718644576480287,
+          "rotation": 11.780972450961727,
           "target": "5-upstairs-living"
         }
       ],
@@ -212,22 +218,22 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -2.8408358059358143,
-        "pitch": 0.09834313010496842,
+        "yaw": -2.3401680248087793,
+        "pitch": -0.035051277854600826,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": -2.0347506624849494,
-          "pitch": 0.19328583315424197,
-          "rotation": 7.0685834705770345,
+          "yaw": -2.001674928079197,
+          "pitch": 0.404657882620155,
+          "rotation": 0.7853981633974483,
           "target": "4-upstairs-workspace"
         },
         {
-          "yaw": -3.0099481563381154,
-          "pitch": 0.2154704246670498,
-          "rotation": 5.497787143782138,
-          "target": "8-bedroom-3"
+          "yaw": -3.081045931711481,
+          "pitch": 0.35101642411067147,
+          "rotation": 10.995574287564278,
+          "target": "8-bedrooms-3--4"
         }
       ],
       "infoHotspots": []
@@ -256,16 +262,16 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -1.657681342154504,
-        "pitch": 0.12263469191416831,
+        "yaw": -2.110345894883949,
+        "pitch": 0.034030896638236996,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 1.1309514868682875,
-          "pitch": 0.09256481940821182,
+          "yaw": 0.5738963078755255,
+          "pitch": 0.2388464850268015,
           "rotation": 4.71238898038469,
-          "target": "1-living-room"
+          "target": "2-rumpus"
         }
       ],
       "infoHotspots": []
@@ -294,61 +300,23 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 3.0314339167973987,
-        "pitch": 0.10578876272717075,
+        "yaw": 2.8764944980032974,
+        "pitch": 0.2028747501076147,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 0.666059019105937,
-          "pitch": 0.06466199379209314,
-          "rotation": 10.995574287564278,
-          "target": "5-upstairs-living"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "8-bedroom-3",
-      "name": "Bedroom 3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "yaw": 1.611979044628642,
-        "pitch": 0.08141175124900712,
-        "fov": 1.3219339076578436
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.328247037836375,
-          "pitch": 0.19731529826834482,
+          "yaw": 0.21548519156769785,
+          "pitch": 0.08512453193252156,
           "rotation": 1.5707963267948966,
-          "target": "4-upstairs-workspace"
+          "target": "2-rumpus"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-bedroom-4",
-      "name": "Bedroom 4",
+      "id": "8-bedrooms-3--4",
+      "name": "Bedrooms 3 & 4",
       "levels": [
         {
           "tileSize": 256,
@@ -370,22 +338,15 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -1.385385940439452,
-        "pitch": -0.07780813290895594,
+        "yaw": -1.230808668505965,
+        "pitch": 0.10753557726213181,
         "fov": 1.3219339076578436
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.6737108410201467,
-          "pitch": 0.18902470206115396,
-          "rotation": 4.71238898038469,
-          "target": "5-upstairs-living"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Unit 4-27-29 Clayton Road Oakleigh East",
+  "name": "Unit 4 - 27-29 Clayton Rd Oakleigh East",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
